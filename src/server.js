ba8b830af04189b138fa34d16c7acb683ac38536
Erik Pablo <nunespablo470@gmail.com>
2025-02-24 09:36:54 -0300

    add get

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.get("/message", (require, response) => {
+    response.send("Hello world, good mooring")
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server ir running on Port ${PORT}`));
