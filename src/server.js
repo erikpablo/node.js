967e986720be855819dfed728469c9a65d5d822a
Erik Pablo <nunespablo470@gmail.com>
2025-02-25 09:13:33 -0300

    query params

--- src/server.js
+++ src/server.js
@@ -11,5 +11,11 @@ app.get("/message/:id/:user", (request, response) => {
     `);
 });
 
+app.get("/user", (request, response) => {
+    const { page, limit } = request.query;
+
+    response.send(`Page: ${page}. Mostrar: ${limit}`)
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
