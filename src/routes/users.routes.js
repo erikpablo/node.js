f21926c6aa95ded3e6ba2da61b96ef91d0a68939
Erik Pablo <nunespablo470@gmail.com>
2025-03-04 23:50:09 -0300

    add middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,8 +3,18 @@ const { Router } = require("express");
 const UsersController = require('../controllers/UsersControllers')
 const usersRoutes = Router();
 
+function myMiddleware(request, response, next ) {
+    console.log("você passou pelo middleware ")
+    if(!request.body.isAdmin) {
+        return response.status(404).json({ message: "you are not authorized" })
+    }
+    next();
+};
+
+
+
 const usersController = new UsersController();
 
-usersRoutes.post('/', usersController.create);
+usersRoutes.post('/', myMiddleware, usersController.create);
 
-module.exports = usersRoutes;
+module.exports = usersRoutes; 
